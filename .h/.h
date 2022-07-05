96eee1d90f081dd0222524c4aa129e86bac6fc57
mohamed mohamed <mohamedmohamed@mohameds-MacBook-Air.local>
2022-07-05 17:07:25 -0500

    html.style

---
+++ .h/.h
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./static/style.css">
+</head>
+<body>
+    <div>
+        <div class="header"></div>
+
+        <div class="left_nav">
+            <li>Home</li>
+            <li>Help</li>
+            <li>Faq</li>
+        </div>
+        <div class="main_content">
+            <h4>Heres my website</h4>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. At accusamus voluptates odio reprehenderit nobis ea! Sunt atque natus, iusto asperiores tempora totam qui ipsum explicabo adipisci! Explicabo quis animi vero!</p>
+        </div>
+    </div>
+</body>
+</html>
